23ae66d08d3212f0bbcd1148b720663019b6ae1d
DenisRaimondi <denis.raimondi@gmail.com>
2024-11-01 09:43:49 +0100

    modificato firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,14 @@
-import { firebaseConfig as testConfig } from './firebase.test';
-import { firebaseConfig as prodConfig } from './firebase.prod';
+import { firebaseConfig as testConfig } from "./firebase.test";
+import { firebaseConfig as prodConfig } from "./firebase.prod";
 
-export const firebaseConfig = process.env.REACT_APP_ENV === 'production' 
-  ? prodConfig 
-  : testConfig;
+export const firebaseConfig =
+  process.env.REACT_APP_ENV === "production" ? prodConfig : testConfig;
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Get Auth and Firestore instances
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export default app;
